bd54e6cf3f174268b750271dbbc3ea48783282b2
macarron001 <rpinoon16@gmail.com>
2022-05-13 11:33:55 +0800

    added controller for article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.description = params[:article][:description]
+
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+end
